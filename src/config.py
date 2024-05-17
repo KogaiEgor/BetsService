507e75a4bcec4a397ef3791d49c225e75ec3dc45
Egor <kogayegor@gmail.com>
2024-05-17 17:09:02 +0300

    change cache, get data from cache

--- src/config.py
+++ src/config.py
@@ -1,7 +1,9 @@
 import os
+import redis
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
 odd_token = os.environ.get("ODD_TOKEN")
+rd = redis.Redis(host='localhost', port=6379, db=0)
--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
 import asyncio
 from fastapi import FastAPI
-from redis import asyncio as aioredis
+from fastapi import HTTPException
 
-from fastapi_cache import FastAPICache
-from fastapi_cache.backends.redis import RedisBackend
-from fastapi_cache.decorator import cache
+import redis
 
+from src.config import rd
 from src.logger import setup_logging
-from src.parser.oddscorp_arbs import get_surebet, get_surebet_pari
+from src.parser.oddscorp_arbs import get_surebet
+
 
 
 app = FastAPI(
@@ -23,8 +23,6 @@ async def get_surebettest():
 
 @app.on_event("startup")
 async def startup_event():
-    redis = aioredis.from_url("redis://localhost")
-    FastAPICache.init(RedisBackend(redis), prefix="fast-api cache")
     asyncio.create_task(cache_arbs())
 
 
@@ -33,20 +31,25 @@ async def cache_arbs():
         result, bet_type, link, koef, koef2, bet_id, mirror_res, match_name = await get_surebettest()
         logger.info(f"Data: {result, bet_type, link, koef, koef2, bet_id, mirror_res, match_name}")
 
-        @cache(expire=60)
-        async def index():
-            logger.debug("Saving arb to cache")
-            return dict(result=result, bet_type=bet_type, link=link,
-                        koef=koef, koef2=koef2, bet_id=bet_id,
-                        mirror_res=mirror_res, match_name=match_name)
+        data = {
+            "result": result,
+            "bet_type": bet_type,
+            "link": link,
+            "koef": koef,
+            "koef2": koef2,
+            "bet_id": bet_id,
+            "mirror_res": mirror_res,
+            "match_name": match_name
+        }
 
-        await index()
+        #rd.hset("last_arb", mapping=data)
+
         await asyncio.sleep(3)
 
 
 @app.get("/arbs/")
-async def get_arb():
-    arb = await get_surebet_pari("soccer")
+async def get_arbs():
+    arb = rd.hgetall("last_arb")
     return arb
 
 
