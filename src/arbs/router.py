7b65a5aabd6090810188e45cb624e1aebaa82f68
Egor <kogayegor@gmail.com>
2024-05-27 13:34:29 +0300

    Error response update

--- src/arbs/router.py
+++ src/arbs/router.py
@@ -1,5 +1,6 @@
 import logging
 from fastapi import APIRouter, status, Response
+from fastapi.responses import JSONResponse
 
 from src.config import rd
 from src.arbs.schemas import Bet
@@ -12,10 +13,11 @@ router = APIRouter(
 logger = logging.getLogger(__name__)
 
 @router.get("/get_arb/", status_code=status.HTTP_200_OK, response_model=Bet)
-async def get_arb(response: Response):
+async def get_arb():
     arb = await rd.hgetall("last_arb")
     if not arb:
-        return []
+        logger.debug("No arbs")
+        return JSONResponse(status_code=status.HTTP_200_OK, content={"message": "No arbs from odd"})
     result = await rd.delete("last_arb")
 
     if result == 0:
