784263a66b7d3c1138922dab96f4312c37f881f6
Egor <kogayegor@gmail.com>
2024-05-29 18:29:02 +0300

    reconnect if ping update

--- src/market/oddscorp_websocket.py
+++ src/market/oddscorp_websocket.py
@@ -21,22 +21,23 @@ message = orjson.dumps(config)
 
 logger = logging.getLogger(__name__)
 async def read_odds_socket():
-    async with websockets.connect(
-        "ws://api.oddscp.com:8001", ping_interval=None
-    ) as websocket:
-        await websocket.send(message)
-        async for msg in websocket:
-            data = orjson.loads(msg)
-            # if isinstance(data, list) and len(data) > 1 and (data[1] in ("update_markets", "remove_event", "remove_markets")):
-            #     print(msg)
-            logger.debug(f" Websocket {msg}")
-            if isinstance(data, dict):
-                if 'last_delay_100avg' in data.keys():
-                    logger.info(F"PING = {data}")
-                    if data['last_delay_100avg'] > 100:
-                        logger.warning("Ping exceeded 100ms, reconnecting...")
-                        await asyncio.sleep(1)
-                        break
-            handler = KoefsHandler(data)
-            await handler.save_message()
+    while True:
+        async with websockets.connect(
+            "ws://api.oddscp.com:8001", ping_interval=None
+        ) as websocket:
+            await websocket.send(message)
+            async for msg in websocket:
+                data = orjson.loads(msg)
+                # if isinstance(data, list) and len(data) > 1 and (data[1] in ("update_markets", "remove_event", "remove_markets")):
+                #     print(msg)
+                logger.debug(f" Websocket {msg}")
+                if isinstance(data, dict):
+                    if 'last_delay_100avg' in data.keys():
+                        logger.info(F"PING = {data}")
+                        if data['last_delay_100avg'] > 100:
+                            logger.warning("Ping exceeded 100ms, reconnecting...")
+                            await asyncio.sleep(1)
+                            break
+                handler = KoefsHandler(data)
+                await handler.save_message()
 
